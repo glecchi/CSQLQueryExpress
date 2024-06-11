bc5f73a7387728a40b70965d56ccdf63e1eb4c31
jED <gianluca.lecchi@hotmail.it>
2024-06-11 21:15:53 +0200

    Improved SQLQuery initialization.

--- Lib/CSQLQueryExpress/SQLQuery.cs
+++ Lib/CSQLQueryExpress/SQLQuery.cs
@@ -8,81 +8,84 @@ namespace CSQLQueryExpress
     /// </summary>
     public sealed class SQLQuery
     {
-        private readonly IList<ISQLQueryFragment> _fragments = new List<ISQLQueryFragment>();
-
         public SQLQueryFrom<T> From<T>() where T : ISQLQueryEntity
         {
-            return new SQLQueryFrom<T>(_fragments);
+            return new SQLQueryFrom<T>(InitNewFragmentsList());
         }
 
         public SQLQueryFrom<T> From<T>(SQLQuerySelect<T> select) where T : ISQLQueryEntity
         {
-            return new SQLQueryFrom<T>(_fragments, select);
+            return new SQLQueryFrom<T>(InitNewFragmentsList(), select);
         }
 
         public SQLQueryFromUnion<T> From<T>(SQLQueryUnion<T> union)
         {
-            return new SQLQueryFromUnion<T>(_fragments, union);
+            return new SQLQueryFromUnion<T>(InitNewFragmentsList(), union);
         }
 
         public SQLQueryInsert<T> Insert<T>(T insert) where T : ISQLQueryEntity
         {
-            return new SQLQueryInsert<T>(_fragments, insert);
+            return new SQLQueryInsert<T>(InitNewFragmentsList(), insert);
         }
 
         public SQLQueryInsert<T> Insert<T>(object insert) where T : ISQLQueryEntity
         {
-            return new SQLQueryInsert<T>(_fragments, insert);
+            return new SQLQueryInsert<T>(InitNewFragmentsList(), insert);
         }
 
         public SQLQueryInsert<T> Insert<T>(IDictionary<string, object> insert) where T : ISQLQueryEntity
         {
-            return new SQLQueryInsert<T>(_fragments, insert);
+            return new SQLQueryInsert<T>(InitNewFragmentsList(), insert);
         }
 
         public SQLQueryUnion<T> Union<T>(SQLQuerySelect<T> firstSelect, SQLQuerySelect<T> secondSelect, params SQLQuerySelect<T>[] otherSelect)
         {
-            return new SQLQueryUnion<T>(_fragments, false, firstSelect, secondSelect, otherSelect);
+            return new SQLQueryUnion<T>(InitNewFragmentsList(), false, firstSelect, secondSelect, otherSelect);
         }
 
         public SQLQueryUnion<T> UnionAll<T>(SQLQuerySelect<T> firstSelect, SQLQuerySelect<T> secondSelect, params SQLQuerySelect<T>[] otherSelect)
         {
-            return new SQLQueryUnion<T>(_fragments, true, firstSelect, secondSelect, otherSelect);
+            return new SQLQueryUnion<T>(InitNewFragmentsList(), true, firstSelect, secondSelect, otherSelect);
         }
 
         public SQLQueryBatch<T> Batch<T>(params SQLQueryInsert<T>[] insert) where T : ISQLQueryEntity
         {
-            return new SQLQueryBatch<T>(_fragments, insert);
+            return new SQLQueryBatch<T>(InitNewFragmentsList(), insert);
         }
 
         public SQLQueryBatch<T> Batch<T>(params SQLQueryUpdate<T>[] update) where T : ISQLQueryEntity
         {
-            return new SQLQueryBatch<T>(_fragments, update);
+            return new SQLQueryBatch<T>(InitNewFragmentsList(), update);
         }
 
         public SQLQueryBatch<T> Batch<T>(params SQLQueryDelete<T>[] delete) where T : ISQLQueryEntity
         {
-            return new SQLQueryBatch<T>(_fragments, delete);
+            return new SQLQueryBatch<T>(InitNewFragmentsList(), delete);
         }
 
         public SQLQueryTruncate<T> Truncate<T>() where T : ISQLQueryEntity
         {
-            return new SQLQueryTruncate<T>(_fragments);
+            return new SQLQueryTruncate<T>(InitNewFragmentsList());
         }
 
         public SQLQueryDrop<T> Drop<T>() where T : ISQLQueryEntity
         {
-            return new SQLQueryDrop<T>(_fragments);
+            return new SQLQueryDrop<T>(InitNewFragmentsList());
         }
 
         public SQLQueryStoredProcedure StoredProcedure(ISQLStoredProcedure procedure)
         {
-            return new SQLQueryStoredProcedure(_fragments, procedure);
+            return new SQLQueryStoredProcedure(InitNewFragmentsList(), procedure);
         }
 
         public SQLQueryStoredProcedure<T> StoredProcedure<T>(ISQLStoredProcedure<T> procedure)
         {
-            return new SQLQueryStoredProcedure<T>(_fragments, procedure);
+            return new SQLQueryStoredProcedure<T>(InitNewFragmentsList(), procedure);
+        }
+
+        private IList<ISQLQueryFragment> InitNewFragmentsList()
+        {
+             return new List<ISQLQueryFragment>();
         }
     }
 }
