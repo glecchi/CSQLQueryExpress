7912ae6a561e8b64d1460b8ffea136fc92110a36
jED <gianluca.lecchi@hotmail.it>
2024-11-20 11:40:23 +0100

    Added SQLQueryCompilerSettings.

---
+++ Lib/CSQLQueryExpress/Compiler/SQLQueryCompilerSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace CSQLQueryExpress
+{
+    public sealed class SQLQueryCompilerSettings
+    {
+        /// <summary>
+        /// The prefix of all query paramenters. Default is @p. Example @p0, @p1.
+        /// </summary>
+        public string QueryParameterPrefix { get; set; } = "@p";
+
+        /// <summary>
+        /// The prefix of all stored procedure paramenters. Default is @. Example @parameterName.
+        /// </summary>
+        public string StoredProcedureParameterPrefix { get; set; } = "@";
+
+        /// <summary>
+        /// The prefix of all query tables alias. Default is _t. Example _t1, _t2.
+        /// </summary>
+        public string TableAliasPrefix { get; set; } = "_t";
+    }
+}
